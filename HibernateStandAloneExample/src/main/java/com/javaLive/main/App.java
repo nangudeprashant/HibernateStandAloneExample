7b38cb35f7fbff9fc768f64176466c849e11ead2
Administrator <Administrator@DELL>
2019-08-21 20:26:30 +0530

    package com.javaLive.service renamed to com.javaLive.main

--- HibernateStandAloneExample/src/main/java/com/javaLive/main/App.java
+++ HibernateStandAloneExample/src/main/java/com/javaLive/main/App.java
@@ -1,13 +1,17 @@
-package com.javaLive.entity;
+package com.javaLive.main;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.javaLive.databaseUtil.*;
+import com.javaLive.entity.Student;
 
 public class App {
     public static void main(String[] args) {
+    	final Logger logger = LoggerFactory.getLogger(App.class);
     	Student student1 = new Student(73,"Name73", "Address73");
         Student student2 = new Student(74,"Name74", "Address74");
         Transaction transaction = null;
@@ -23,6 +27,7 @@ public class App {
             // commit transaction
             transaction.commit();
             System.out.println("Displaying student list after inserting new entires");
+            logger.info("Displaying student list after inserting new entires");
             new App().getStudentList();
             // update the student objects
             student1.setAddress("NewName73");
